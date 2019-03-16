6deb6252e20903189940c48aabc642768e4229e5
MrMouse <it@asygnata.com>
2019-03-16 21:47:02 +0100

    prosta autoryzacja haslo niezaszyfrowane

--- src/main/java/org/pl/komis/auth/Authorization.java
+++ src/main/java/org/pl/komis/auth/Authorization.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class Authorization {
     private String password = "pass1234";
 
+    //sprawdź poprawność hasła
     public boolean checkAuthorization(String password){
         return this.password.equals(password);
     }
 
+    //pobierz hasło od usera
     public String getPassword(){
         Scanner input = new Scanner(System.in);
         System.out.println("Podaj hasło:");
