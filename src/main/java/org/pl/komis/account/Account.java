2d4afdef28b34d104e683be03d945a9a90f0c895
MrMouse <it@asygnata.com>
2019-03-16 22:14:58 +0100

    stan konta zapisany w pliku

--- src/main/java/org/pl/komis/account/Account.java
+++ src/main/java/org/pl/komis/account/Account.java
@@ -1,10 +1,11 @@
 package org.pl.komis.account;
 
+import java.io.*;
 import java.math.BigDecimal;
 
 public class Account {
     //konto bankowe komisu
-    public static BigDecimal account = new BigDecimal(0);
+    private static BigDecimal account = new BigDecimal(0);
 
     public static BigDecimal getAccount() {
         return account;
@@ -12,5 +13,55 @@ public class Account {
 
     public static void setAccount(BigDecimal account) {
         Account.account = account;
+    }
+
+
+
+    public static void saveAccount() {
+        try {
+            //przygotowanie pliku
+            PrintWriter writer = new PrintWriter(new FileWriter("account.txt"));
+
+            //zapisz stan konta do pliku
+            writer.println(account);
+
+            //zamknięcie pliku
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void loadAccount() {
+        String state = new String();
+        try {
+            //przygotowanie pliku
+            BufferedReader read = new BufferedReader(new FileReader("account.txt"));
+
+            //wczytaj stan konta
+            state = read.readLine();
+
+            //zamknięcie pliku
+            read.close();
+
+        } catch (FileNotFoundException e) {
+            state="0";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (!state.equals(null)) {
+            account = new BigDecimal(state);
+        } else {
+            account = new BigDecimal(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        loadAccount();
+        System.out.println(account);
+        setAccount(getAccount().add(new BigDecimal(123)));
+        saveAccount();
     }
 }
