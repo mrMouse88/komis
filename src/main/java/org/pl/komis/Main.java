3a87965e63f8ab26dcf9e30a67cf26ffd9eae8d3
mrMouse <ia@asygnata.com>
2019-03-17 10:28:27 +0100

    czyszczenie

--- src/main/java/org/pl/komis/Main.java
+++ src/main/java/org/pl/komis/Main.java
@@ -1,14 +1,8 @@
 package org.pl.komis;
 
-import org.pl.komis.account.Account;
 import org.pl.komis.auth.Authorization;
-import org.pl.komis.car.Car;
-import org.pl.komis.car.CarList;
-import org.pl.komis.car.CarType;
 import org.pl.komis.menu.Menu;
-import org.pl.komis.menu.MenuUtils;
 
-import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
